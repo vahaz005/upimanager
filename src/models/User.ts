b650b5f834bae857e5370324f07c2d3d8d1d6f69
vahaz005 <150015676+vahaz005@users.noreply.github.com>
2024-07-20 16:08:01 +0530

    vahaz

---
+++ src/models/User.ts
@@ -0,0 +1,32 @@
+import { Schema, model, Document } from 'mongoose';
+import mongoose from "mongoose"
+
+interface IUser extends Document {
+  fullName: string;
+  email: string;
+  password: string;
+  postalCode: string;
+  state: string;
+  ssn: string;
+  dateOfBirth: Date;
+  address: string;
+}
+
+const userSchema = new Schema<IUser>({
+  fullName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  postalCode: { type: String, required: true },
+  state: { type: String, required: true },
+  ssn: { type: String, required: true, unique: true },
+  dateOfBirth: { type: Date, required: true },
+  address: { type: String, required: true },
+});
+
+const User = mongoose.models.User as mongoose.Model<IUser> ||
+
+
+
+mongoose.model<IUser>('User', userSchema);
+
+export default User;
